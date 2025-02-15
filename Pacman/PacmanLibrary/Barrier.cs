9844eb226f8c6140e272207939b33e9bfa4fed3d
Малихіна Ангеліна Олегівна <vt231_mao@student.ztu.edu.ua>
2025-02-15 09:49:30 +0200

    Initial commit

---
+++ Pacman/PacmanLibrary/Barrier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PacmanLibrary
+{
+    public class Barrier : Element
+    {
+        public Barrier(int x, int y) : base(x, y)
+        { }
+    }
+}
---
+++ Pacman/PacmanLibrary/GraphicPoint.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacmanLibrary
+{
+    public class GraphicPoint
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Bitmap Sprite { get; set; } = null;
+        
+        public GraphicPoint(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
